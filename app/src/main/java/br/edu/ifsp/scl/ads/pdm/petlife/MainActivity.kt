c36d7f3ab1e8c0bc518078f8ba58f8cfdfcd7a75
Daniel Ferri <89649179+danferri@users.noreply.github.com>
2024-10-10 11:04:18 -0300

    feat: project structure and some configs add

---
+++ app/src/main/java/br/edu/ifsp/scl/ads/pdm/petlife/MainActivity.kt
@@ -0,0 +1,25 @@
+package br.edu.ifsp.scl.ads.pdm.petlife
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import br.edu.ifsp.scl.ads.pdm.petlife.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val amb: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(amb.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
