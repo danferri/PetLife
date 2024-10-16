58574daad6431c0db33c3126a70517105ecb60f7
Daniel Ferri <89649179+danferri@users.noreply.github.com>
2024-10-16 17:55:24 -0300

    fix: rename class LasPetshopVisitActivity to LastPetshopVisitActivity.

--- app/src/main/java/br/edu/ifsp/scl/ads/pdm/petlife/LastPetshopVisitActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/pdm/petlife/LastPetshopVisitActivity.kt
@@ -6,11 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class LasPetshopVisitActivity : AppCompatActivity() {
+class LastPetshopVisitActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_las_petshop_visit)
+        setContentView(R.layout.`activity_last_petshop_visit.xml`)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
