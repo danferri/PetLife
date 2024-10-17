3487b5c399257db57a471b5c8d6ec0cb68c1576b
Daniel Ferri <89649179+danferri@users.noreply.github.com>
2024-10-17 11:52:58 -0300

    feat: create edition on PetInfoActivity.

--- app/src/main/java/br/edu/ifsp/scl/ads/pdm/petlife/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/pdm/petlife/MainActivity.kt
@@ -20,7 +20,22 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+    companion object Constantes {
+        const val NAME = "NAME"
+        const val BIRTH_DATE = "BIRTH_DATE"
+        const val TYPE = "TYPE"
+        const val COLOR = "COLOR"
+        const val SIZE = "SIZE"
+        const val LAST_VET_VISIT = "LAST_VET_VISIT"
+        const val LAST_VACCINATION = "LAST_VACCINATION"
+    }
+
     private lateinit var pet: Pet
+
+    private lateinit var pil: ActivityResultLauncher<Intent>
+    private lateinit var lvvl: ActivityResultLauncher<Intent>
+    private lateinit var lvl: ActivityResultLauncher<Intent>
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,6 +52,49 @@ class MainActivity : AppCompatActivity() {
             "15/07/2024", "23/07/2024", "10/09/2024"
         )
         updateUI()
+
+        pil = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                result.data?.getStringExtra(NAME)?.let {
+                    amb.petNameTv2.text = it
+                }
+
+                result.data?.getStringExtra(BIRTH_DATE)?.let {
+                    amb.petNameTv2.text = it
+                }
+
+                result.data?.getStringExtra(TYPE)?.let {
+                    amb.petTypeTv2.text = it
+                }
+
+                result.data?.getStringExtra(COLOR)?.let {
+                    amb.petColorTv2.text = it
+                }
+
+                result.data?.getStringExtra(SIZE)?.let {
+                    amb.petSizeTv2.text = it
+                }
+                updateUI()
+            }
+        }
+
+        lvvl = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if(result.resultCode == RESULT_OK) {
+               result.data?.getStringExtra(LAST_VET_VISIT)?.let {
+                   amb.lastVetVisitTv2.text = it
+               }
+               updateUI()
+            }
+        }
+
+        lvl = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if(result.resultCode == RESULT_OK) {
+                result.data?.getStringExtra(LAST_VACCINATION)?.let {
+                    amb.lastVaccinationTv2.text = it
+                }
+                updateUI()
+            }
+        }
     }
 
     private fun updateUI() {
@@ -59,29 +117,29 @@ class MainActivity : AppCompatActivity() {
         return when (item.itemId) {
             R.id.petInfoMi -> {
                 val petData = Intent(this, PetInfoActivity::class.java).apply {
-                    putExtra("name", pet.name)
-                    putExtra("birthDate", pet.birthDate)
-                    putExtra("type", pet.type)
-                    putExtra("color", pet.color)
-                    putExtra("size", pet.size)
+                    putExtra(NAME, pet.name)
+                    putExtra(BIRTH_DATE, pet.birthDate)
+                    putExtra(TYPE, pet.type)
+                    putExtra(COLOR, pet.color)
+                    putExtra(SIZE, pet.size)
                 }
-                startActivity(petData)
+                pil.launch(petData)
                 true
             }
 
             R.id.lastVetVisitMi -> {
                 val lastVetVisit = Intent(this, LastPetVetVisitActivity::class.java).apply {
-                    putExtra("lastVetVisit", pet.lastVetVisit)
+                    putExtra(LAST_VET_VISIT, pet.lastVetVisit)
                 }
-                startActivity(lastVetVisit)
+                lvvl.launch(lastVetVisit)
                 true
             }
 
             R.id.lastVaccinationMi -> {
                 val lastVaccination = Intent(this, LastVaccinationActivity::class.java).apply {
-                    putExtra("lastVaccination", pet.lastVaccination)
+                    putExtra(LAST_VACCINATION, pet.lastVaccination)
                 }
-                startActivity(lastVaccination)
+                lvl.launch(lastVaccination)
                 true
             }
 
--- app/src/main/java/br/edu/ifsp/scl/ads/pdm/petlife/PetInfoActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/pdm/petlife/PetInfoActivity.kt
@@ -1,8 +1,11 @@
 package br.edu.ifsp.scl.ads.pdm.petlife
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import br.edu.ifsp.scl.ads.pdm.petlife.MainActivity.Constantes.BIRTH_DATE
+import br.edu.ifsp.scl.ads.pdm.petlife.MainActivity.Constantes.NAME
 import br.edu.ifsp.scl.ads.pdm.petlife.databinding.ActivityMainBinding
 import br.edu.ifsp.scl.ads.pdm.petlife.databinding.ActivityPetInfoBinding
 
@@ -21,17 +24,53 @@ class PetInfoActivity : AppCompatActivity() {
             subtitle = "Dados do Pet" //this@MainActivity.javaClass.simpleName
         }
 
-        val petName = intent.getStringExtra("name")
-        val petBirthDate = intent.getStringExtra("birthDate")
-        val petType = intent.getStringExtra("type")
-        val petColor = intent.getStringExtra("color")
-        val petSize = intent.getStringExtra("size")
+        intent.getStringExtra(MainActivity.NAME)?.also{ parametro ->
+            piab.petNameEt.setText(parametro)
+        }
 
-        piab.petNameEt.setText(petName)
-        piab.petBirthDateEt.setText(petBirthDate)
-        piab.petTypeEt.setText(petType)
-        piab.petColorEt.setText(petColor)
-        piab.petSizeEt.setText(petSize)
+        intent.getStringExtra(MainActivity.BIRTH_DATE)?.also{ parametro ->
+            piab.petBirthDateEt.setText(parametro)
+        }
+
+        intent.getStringExtra(MainActivity.TYPE)?.also{ parametro ->
+            piab.petTypeEt.setText(parametro)
+        }
+
+        intent.getStringExtra(MainActivity.COLOR)?.also{ parametro ->
+            piab.petColorEt.setText(parametro)
+        }
+
+        intent.getStringExtra(MainActivity.SIZE)?.also{ parametro ->
+            piab.petSizeEt.setText(parametro)
+        }
+
+        piab.saveBt.setOnClickListener {
+            Intent().apply {
+                piab.petNameEt.text.toString().let{
+                    this.putExtra(NAME, it)
+                }
+
+                piab.petBirthDateEt.text.toString().let{
+                    this.putExtra(BIRTH_DATE, it)
+                }
+                setResult(RESULT_OK, this)
+            }
+            finish()
+        }
+
+
+
+//        val petName = intent.getStringExtra(MainActivity.NAME)
+//        val petBirthDate = intent.getStringExtra("birthDate")
+//        val petType = intent.getStringExtra("type")
+//        val petColor = intent.getStringExtra("color")
+//        val petSize = intent.getStringExtra("size")
+//
+//        piab.petNameEt.setText(petName)
+//        piab.petBirthDateEt.setText(petBirthDate)
+//        piab.petTypeEt.setText(petType)
+//        piab.petColorEt.setText(petColor)
+//        piab.petSizeEt.setText(petSize)
 
     }
 
