a2a5e1c1a1b01cf3c8cd35252d77c802b31c6ddf
Daniel Ferri <89649179+danferri@users.noreply.github.com>
2024-12-07 20:51:10 -0300

    feat: create EventListActivity and activity_event_list.xml and implement some logic.

---
+++ app/src/main/java/br/edu/ifsp/scl/ads/pdm/petlife/ui/EventListActivity.kt
@@ -0,0 +1,96 @@
+package br.edu.ifsp.scl.ads.pdm.petlife.ui
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.ContextMenu
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.AdapterView.AdapterContextMenuInfo
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResultLauncher
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import br.edu.ifsp.scl.ads.pdm.petlife.R
+import br.edu.ifsp.scl.ads.pdm.petlife.databinding.ActivityEventListBinding
+import br.edu.ifsp.scl.ads.pdm.petlife.model.Constant.PET
+import br.edu.ifsp.scl.ads.pdm.petlife.model.Event
+import br.edu.ifsp.scl.ads.pdm.petlife.model.Pet
+
+class EventListActivity : AppCompatActivity() {
+    private val aelb: ActivityEventListBinding by lazy {
+        ActivityEventListBinding.inflate(layoutInflater)
+    }
+
+    // Data source
+    private val eventList: MutableList<Event> = mutableListOf()
+
+    // Adapter
+    private val eventAdapter: EventAdapter by lazy {
+        EventAdapter(this, eventList)
+    }
+
+    private lateinit var earl: ActivityResultLauncher<Intent>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(aelb.root)
+
+
+        aelb.toolbarIn.toolbar.let {
+            it.subtitle = "Event List"
+            setSupportActionBar(it)
+        }
+
+        aelb.eventsLv.adapter = eventAdapter
+
+
+        registerForContextMenu(aelb.eventsLv)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.event_add_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean = when(item.itemId) {
+        R.id.addEventMi -> {
+            earl.launch(Intent(this, EventActivity::class.java))
+            true
+        }
+
+        else -> {
+            false
+        }
+    }
+
+    override fun onCreateContextMenu(
+        menu: ContextMenu?,
+        v: View?,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) = menuInflater.inflate(R.menu.context_event_menu, menu)
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        val position = (item.menuInfo as AdapterContextMenuInfo).position
+
+        return when(item.itemId) {
+            R.id.editEventMi -> {
+                Intent(this, EventActivity::class.java).apply {
+                    putExtra(PET, eventList[position])
+                    earl.launch(this)
+                }
+                true
+            }
+            R.id.removePetMi -> {
+                eventList.removeAt(position)
+                eventAdapter.notifyDataSetChanged()
+                true
+            }
+            else -> {
+                false
+            }
+        }
+    }
+}
