da3f95153dd8500c1b80fa9f7fa08d890121efeb
Daniel Ferri <89649179+danferri@users.noreply.github.com>
2024-12-08 14:10:38 -0300

    feat: create PetDao interface to manage Pet operations in the data base.

---
+++ app/src/main/java/br/edu/ifsp/scl/ads/pdm/petlife/model/PetDao.kt
@@ -0,0 +1,9 @@
+package br.edu.ifsp.scl.ads.pdm.petlife.model
+
+interface PetDao {
+    fun createPet(pet: Pet): Long
+    fun create(name: String): Pet
+    fun retrievePet(): MutableList<Pet>
+    fun updatePet(pet: Pet): Int
+    fun deletePet(name: String): Int
+}
