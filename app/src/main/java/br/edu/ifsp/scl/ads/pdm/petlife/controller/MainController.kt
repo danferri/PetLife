dead5d929f90ab7b35eee4152eabab3963ec8ae0
Daniel Ferri <89649179+danferri@users.noreply.github.com>
2024-12-08 16:28:05 -0300

    feat: create MainController to manage manage business logic.

---
+++ app/src/main/java/br/edu/ifsp/scl/ads/pdm/petlife/controller/MainController.kt
@@ -0,0 +1,18 @@
+package br.edu.ifsp.scl.ads.pdm.petlife.controller
+
+import br.edu.ifsp.scl.ads.pdm.petlife.model.Pet
+import br.edu.ifsp.scl.ads.pdm.petlife.model.PetDao
+import br.edu.ifsp.scl.ads.pdm.petlife.model.PetSqLiteImpl
+import br.edu.ifsp.scl.ads.pdm.petlife.ui.MainActivity
+
+class MainController(mainActivity: MainActivity) {
+    private val petDao: PetDao = PetSqLiteImpl(mainActivity)
+
+    fun insertPet(pet: Pet) = petDao.createPet(pet)
+    fun getPet(name: String) = petDao.retrievePet(name)
+    fun getPet() = petDao.retrievePet()
+    fun modifyPet(pet: Pet) = petDao.updatePet(pet)
+    fun removePet(name: String) = petDao.deletePet(name)
+
+
+}
