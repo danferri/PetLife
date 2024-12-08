e1778dfcdb23e70c34250fab7565a1336fa11958
Daniel Ferri <89649179+danferri@users.noreply.github.com>
2024-12-08 18:51:02 -0300

    feat: finish implementation of EventSqLiteImpl.

--- app/src/main/java/br/edu/ifsp/scl/ads/pdm/petlife/dao/EventSqLiteImpl.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/pdm/petlife/dao/EventSqLiteImpl.kt
@@ -3,10 +3,12 @@ package br.edu.ifsp.scl.ads.pdm.petlife.dao
 import android.content.ContentValues
 import android.content.Context
 import android.content.Context.MODE_PRIVATE
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteException
 import android.util.Log
 import br.edu.ifsp.scl.ads.pdm.petlife.model.Event
+import br.edu.ifsp.scl.ads.pdm.petlife.model.PetEvent
 
 class EventSqLiteImpl(context: Context): EventDao {
     companion object {
@@ -41,10 +43,58 @@ class EventSqLiteImpl(context: Context): EventDao {
 
     override fun createEvent(event: Event) = eventDatabase.insert(EVENT_TABLE, null, eventToContentValues(event))
 
+    override fun retrieveEvent(id: Int): Event {
+        val cursor = eventDatabase.query(
+            true,
+            EVENT_TABLE,
+            null,
+            "$ID_COLUMN = ?",
+            arrayOf(id.toString()),
+            null,
+            null,
+            null,
+            null
+        )
 
+        return if (cursor.moveToFirst()) {
+            cursorToEvent(cursor)
+        } else {
+            Event()
+        }
+    }
 
+    override fun retrieveEvents(): MutableList<Event> {
+        val eventList = mutableListOf<Event>()
 
+        val cursor = eventDatabase.rawQuery("SELECT * FROM $EVENT_TABLE", null)
+        while (cursor.moveToNext()) {
+            eventList.add(cursorToEvent(cursor))
+        }
+        return eventList
+    }
 
+    override fun retrieveEventsFromPet(petName: String): MutableList<Event> {
+        val eventList = mutableListOf<Event>()
+
+        val cursor = eventDatabase.rawQuery("SELECT * FROM $EVENT_TABLE WHERE $PET_NAME_COLUMN", null)
+        while (cursor.moveToNext()) {
+            eventList.add(cursorToEvent(cursor))
+        }
+        return eventList
+    }
+
+    override fun updateEvent(event: Event) =  eventDatabase.update(
+        EVENT_TABLE,
+        eventToContentValues(event),
+        "$ID_COLUMN = ?",
+        arrayOf(event.id.toString())
+    )
+
+    override fun deleteEvent(id: Int) = eventDatabase.delete(
+        EVENT_TABLE,
+        "$ID_COLUMN = ?",
+        arrayOf(id.toString())
+    )
 
     private fun eventToContentValues(event: Event) = ContentValues().apply {
         with(event) {
@@ -54,6 +104,16 @@ class EventSqLiteImpl(context: Context): EventDao {
             put(DESCRIPTION_COLUMN, description)
             put(PET_NAME_COLUMN, petName)
         }
+    }
+
+    private fun cursorToEvent(cursor: Cursor) = with(cursor) {
+        Event(
+            getInt(getColumnIndexOrThrow(ID_COLUMN)),
+            getString(getColumnIndexOrThrow(PET_NAME_COLUMN)),
+            PetEvent.fromString(getString(getColumnIndexOrThrow(PET_EVENT_COLUMN))),
+            getString(getColumnIndexOrThrow(EVENT_DATE_COLUMN)),
+            getString(getColumnIndexOrThrow(DESCRIPTION_COLUMN))
+        )
     }
 
 }
