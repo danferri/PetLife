2b1b47d1a6bd41cbf003357aae511021d39ce5cf
Daniel Ferri <89649179+danferri@users.noreply.github.com>
2024-12-08 20:00:52 -0300

    feat: add logic in EventController.

--- app/src/main/java/br/edu/ifsp/scl/ads/pdm/petlife/controller/EventController.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/pdm/petlife/controller/EventController.kt
@@ -1,4 +1,15 @@
 package br.edu.ifsp.scl.ads.pdm.petlife.controller
 
-class EventController {
+import br.edu.ifsp.scl.ads.pdm.petlife.dao.EventDao
+import br.edu.ifsp.scl.ads.pdm.petlife.dao.EventSqLiteImpl
+import br.edu.ifsp.scl.ads.pdm.petlife.model.Event
+import br.edu.ifsp.scl.ads.pdm.petlife.model.Pet
+import br.edu.ifsp.scl.ads.pdm.petlife.ui.EventListActivity
+
+class EventController (eventListActivity: EventListActivity){
+    private val eventDao: EventDao = EventSqLiteImpl(eventListActivity)
+
+    fun insertEvent(event: Event) = eventDao.createEvent(event)
+    fun modifyEvent(event: Event) = eventDao.updateEvent(event)
+    fun removeEvent(id: Int) = eventDao.deleteEvent(id)
 }
