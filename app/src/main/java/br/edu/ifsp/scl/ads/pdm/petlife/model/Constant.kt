0fef8abe6066f4278b884512855d96117a2aec6c
Daniel Ferri <89649179+danferri@users.noreply.github.com>
2024-12-07 21:28:57 -0300

    feat: create some logic in EventActivity.

--- app/src/main/java/br/edu/ifsp/scl/ads/pdm/petlife/model/Constant.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/pdm/petlife/model/Constant.kt
@@ -2,4 +2,5 @@ package br.edu.ifsp.scl.ads.pdm.petlife.model
 
 object Constant {
     const val PET = "PET"
+    const val EVENT = "EVENT"
 }
--- app/src/main/java/br/edu/ifsp/scl/ads/pdm/petlife/ui/EventActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/pdm/petlife/ui/EventActivity.kt
@@ -1,21 +1,67 @@
 package br.edu.ifsp.scl.ads.pdm.petlife.ui
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import br.edu.ifsp.scl.ads.pdm.petlife.R
+import br.edu.ifsp.scl.ads.pdm.petlife.databinding.ActivityEventBinding
+import br.edu.ifsp.scl.ads.pdm.petlife.model.Constant
+import br.edu.ifsp.scl.ads.pdm.petlife.model.Constant.EVENT
+import br.edu.ifsp.scl.ads.pdm.petlife.model.Event
+import br.edu.ifsp.scl.ads.pdm.petlife.model.PetEvent
+import br.edu.ifsp.scl.ads.pdm.petlife.model.PetType
 
 class EventActivity : AppCompatActivity() {
+    private val aeb: ActivityEventBinding by lazy {
+        ActivityEventBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_event)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(aeb.root)
+
+        val eventTypeAdapter = ArrayAdapter<PetEvent>(this,android.R.layout.simple_spinner_item,
+            PetEvent.entries.toTypedArray()
+        )
+        eventTypeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        aeb.eventTypeSp.adapter = eventTypeAdapter
+
+        val receivedEvent = intent.getParcelableExtra<Event>(EVENT)
+        receivedEvent?.let { event ->
+            with(aeb) {
+                with(event) {
+                    val eventIndex = eventTypeAdapter.getPosition(petEvent)
+                    eventTypeSp.setSelection(eventIndex)
+
+                    eventDateEt.setText(eventDate)
+                    eventDescriptionEt.setText(description)
+                }
+            }
+        }
+
+        aeb.toolbarIn.toolbar.let {
+            it.subtitle = if (receivedEvent == null) "New event" else "Edit event"
+            setSupportActionBar(it)
+        }
+
+        aeb.run{
+            saveBt.setOnClickListener {
+                val newEvent = Event(
+                    eventTypeAdapter.getItem(eventTypeSp.selectedItemId.toInt()) ?: PetEvent.VET_VISIT,
+                    eventDateEt.text.toString(),
+                    eventDescriptionEt.text.toString()
+                ).let { event ->
+                    Intent().apply {
+                        putExtra(Constant.EVENT, event)
+                        setResult(RESULT_OK, this)
+                        finish()
+                    }
+                }
+            }
         }
     }
 }
